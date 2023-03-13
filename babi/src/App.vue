770fd92a9695ead2a13c400e6499b9e359028154
eldero-coder <elohounkponromeo@gmail.com>
2023-03-13 11:32:46 +0000

    First commit

---
+++ babi/src/App.vue
@@ -0,0 +1,18 @@
+
+<script>
+import Registre from './components/Registre.vue'
+  export default {
+    name: 'App',
+    components: {
+      Registre
+    }
+  }
+  </script>
+
+
+
+<template>
+  <Registre></Registre>
+  
+
+</template>
